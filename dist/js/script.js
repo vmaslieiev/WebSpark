ab3d2bdbf945bc7bc466172a2fc9c92cd4ee9292
Vadym Maslieiev <51130214+vmaslieiev@users.noreply.github.com>
2021-11-19 16:29:43 +0200

    add new js file

--- dist/js/script.js
+++ dist/js/script.js
@@ -74,3 +74,20 @@ $(function($) {
     galleryOn.removeClass('ds-none');
   })
 });
+$('#more-btn').on('click', function() {
+    let divChildren = $('.gallery').children(':random').clone();
+    let divGridChildren = $('.gallery-grid').children(':random').clone();
+    console.log('click klack', divChildren);
+    // if ( ($('.gallery')).is(':visible') || ($('.gallery-grid')).is(':visible')) {
+    //     $('.gallery').hide('slow')
+    //     $('.gallery-grid').hide('slow')
+    //     $(this).html('Show')
+    // } else {
+    //     $('.gallery').show('slow');
+    //     $('.gallery-grid').show('slow');
+    //     $(this).html('Hide');
+    // }
+    $('.gallery').append(divChildren);
+    $('.gallery-grid').append(divGridChildren);
+    $('.gallery-grid').css('display', 'flex');
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
 @@include('datepicker.js');
 @@include('grid-list-toggle.js');
 @@include('content-toggle.js');
+@@include('load-more.js');
